9b104fca547f68869b3e75b52fc378238fac14c9
Klaus Thevessen <nikolaus07@users.noreply.github.com>
2022-11-21 20:50:26 +0100

    zwei beispiele a) Menüs mit html+css  b) mit jQuery

---
+++ js/getEmail.js
@@ -0,0 +1,4 @@
+var prefix = "kontakt";
+var domain = "mustermann.ch";
+document.write('<a class=\"link\" href=\"mailto:' + prefix + '@' + domain + '\">');
+document.write(prefix + '@' + domain + '</a>');
